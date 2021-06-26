19723a867691e0799ce4fb64b6201736e3a48cd5
madness4love <48094567+madness4love@users.noreply.github.com>
2021-06-26 14:22:01 +0300

    task 4.3

--- src/main/kotlin/ru.netology.pusher/contacts.kt
+++ src/main/kotlin/ru.netology.pusher/contacts.kt
@@ -1,4 +1,4 @@
 package ru.netology.pusher
 
 const val dbUrl = ""
-const val token = "exSfOYS7TKSOt4FvIeksRP:APA91bHC7QBpAkvFr9Ew52-19jS2JtxFR0zCjvjvTd6_KzJABG7TZDqLT48dbxI5JjBxUXquyeWOiceAa0GFwbxzv6ldWoZRP7clP3BbFQxvWXVUwCUCJngghrPhzpX_hJDfM5qEAHZo"
+const val token = "e2RivNS6QAOX_qvSwT_2-d:APA91bGhD5W9UUIqLVJTq-PZCvIJcfd1jqkjy33lfbaIRiS8hafFe7Sxk5zAi7DQ0hC49XaHAUn3AvAJ5_rEtep__p1b1nYv-aqmAA2XJSxP5OgJkOZU0i_SxWBJUJYnu_w5fgiGFqms"
--- src/main/kotlin/ru.netology.pusher/mainfile.kt
+++ src/main/kotlin/ru.netology.pusher/mainfile.kt
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseApp
 import com.google.firebase.FirebaseOptions
 import com.google.firebase.messaging.FirebaseMessaging
 import com.google.firebase.messaging.Message
+import org.apache.commons.logging.Log
 import java.io.FileInputStream
 
 
@@ -22,34 +23,33 @@ fun main() {
 
 fun sendNotificationLike() {
     val message = Message.builder()
-            .putData("action", "LIKE")
-            .putData(
-                "content", """{
+        .putData("action", "LIKE")
+        .putData(
+            "content", """{
           "userId": 1,
           "userName": "Vasiliy",
           "postId": 2,
           "postAuthor": "Netology"
         }""".trimIndent()
-            )
-            .setToken(token)
-            .build()
+        )
+        .setToken(token)
+        .build()
 
 
     FirebaseMessaging.getInstance().send(message)
 }
 
 fun sendNotificationNewPost() {
-   val message = Message.builder()
+
+    val message = Message.builder()
         .putData("action", "NEW_POST")
         .putData(
             "content", """{
                 "userId": 1,
-          "userName": "Vasiliy",
-          "postId": 2,
-                "postContent": "Значимость этих проблем настолько очевидна, что консультация с широким активом играет важную роль в формировании новых предложений. Равным образом рамки и место обучения кадров влечет за собой процесс внедрения и модернизации системы обучения кадров, соответствует насущным потребностям. Товарищи! сложившаяся структура организации представляет собой интересный эксперимент проверки направлений прогрессивного развития.
-"Идейные соображения высшего порядка, а также начало повседневной работы по формированию позиции позволяет оценить значение модели развития. ",
-
-        }""".trimIndent()
+                "userName": "Vasiliy",
+                "postId": 2,
+                "postContent": "Если у вас есть какие то интересные предложения, обращайтесь! Студия Web-Boss всегда готова решить любую задачу. Товарищи! сложившаяся структура организации представляет собой интересный эксперимент проверки направлений прогрессивного развития."
+     }""".trimIndent()
         )
         .setToken(token)
         .build()
